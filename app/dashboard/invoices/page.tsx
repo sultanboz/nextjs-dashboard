bb7d4c3adcb09ccf450afd2d5d82b25a4ea50687
Sultan Bozkurt <sbozkurt@nuevo.com.tr>
2024-10-30 19:52:55 +0300

    invoices comment line

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,10 +1,10 @@
-import Pagination from '@/app/ui/invoices/pagination';
+// import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
+// import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
  
 export default async function Page() {
   return (
